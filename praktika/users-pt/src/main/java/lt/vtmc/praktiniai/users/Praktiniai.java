be4231567738b1ecbdb9e283893cb189efc61050
lukas622 <bsds356@proton.me>
2025-01-15 14:00:50 +0400

    11 kolekcijos

--- praktika/users-pt/src/main/java/lt/vtmc/praktiniai/users/Praktiniai.java
+++ praktika/users-pt/src/main/java/lt/vtmc/praktiniai/users/Praktiniai.java
@@ -1,36 +1,83 @@
 package lt.vtmc.praktiniai.users;
 
+import java.util.Comparator;
 import java.util.List;
 
 public class Praktiniai {
 
     public static Integer countUsersOlderThen25(List<User> users) {
-        throw new UnsupportedOperationException("Not implemented");
+        int counter = 0;
+
+        for (User user : users) {
+            if (user.getAge() > 25) counter++;
+        }
+
+        return counter;
     }
 
     public static double getAverageAge(List<User> users) {
+        int ageSum = 0;
+        int totalUsers = users.size();
 
-        throw new UnsupportedOperationException("Not implemented");
+        for (User user : users) {
+            ageSum += user.getAge();
+        }
+
+        return (double) ageSum/totalUsers;
     }
 
     public static Integer getMinAge(List<User> users) {
-        throw new UnsupportedOperationException("Not implemented");
+        double minage = Double.POSITIVE_INFINITY;
+
+        for (User user : users) {
+            if (user.getAge() < minage) minage = user.getAge();
+        }
+
+        return (int) minage;
     }
 
     public static User findByName(List<User> users, String name) {
-        throw new UnsupportedOperationException("Not implemented");
+        for (User user : users) {
+            if(user.getName().equals(name)) {
+                return user;
+            }
+        }
+        return null;
     }
 
     public static List<User> sortByAge(List<User> users) {
-        throw new UnsupportedOperationException("Not implemented");
+        users.sort(new Comparator<User>() {
+            @Override
+            public int compare(User first, User second) {
+                return first.getAge().compareTo(second.getAge());
+            }
+        });
+
+        return users;
     }
 
     public static User findOldest(List<User> users) {
-        throw new UnsupportedOperationException("Not implemented");
+        User oldest = null;
+        int age = 0;
+
+        for (User user : users) {
+            if (user.getAge() > age) {
+                oldest = user;
+                age = user.getAge();
+            }
+         }
+
+        return oldest;
     }
 
     public static int sumAge(List<User> users) {
-        throw new UnsupportedOperationException("Not implemented");
+        int ageSum = 0;
+
+        for (User user : users) {
+            ageSum += user.getAge();
+        }
+
+        return ageSum;
     }
 
 }
